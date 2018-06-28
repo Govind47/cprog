c588671003d593de25a826e2173e754149be0255
Govind47 <35171230+Govind47@users.noreply.github.com>
2018-06-28 14:11:12 +0530

    Update summ.c

--- summ.c
+++ summ.c
@@ -16,4 +16,16 @@ int main()
         temp=temp+sum;}
         printf("%d\n",temp);
 }
+/*Sum of Sums of Digits in Cyclic Order
+Alex has been 
+asked by his teacher to do an assignment on sums of digits of a number. 
+The assignment requires Alex to find the sum of sums of digits of a 
+given number, as per the method mentioned below.
+If the given number 
+is 582109, the sum of sums of digits will be calculated 
+as=(5)+(5+8)+(5+8+2)+(5+8+2+1) +(5+8+2+1+0) +(5+8+2+1+0+9)
+=5+13+15+16+16+25=90
+Assumptions:
+ For the assignment let us assume that the given number will always 
+contain more than 1 digit i.e. the given number will always be>9*/
 
